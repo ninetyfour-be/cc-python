95378d50b40c1243e29f798bd556d1305079f8f3
Ninety Four <ninetyfour.be@gmail.com>
2022-07-09 12:29:34 +0200

    Add GUI option

---
+++ {{ cookiecutter.__project_uid }}/src/{{ cookiecutter.__package_name }}/{% if cookiecutter.gui -%} gui {%- endif %}/__init__.py
@@ -0,0 +1,5 @@
+"""Graphical interface for {{ cookiecutter.project_name }}."""
+from {{ cookiecutter.__package_name }}.gui.app import App
+
+
+__all__ = ["App"]
---
+++ {{ cookiecutter.__project_uid }}/src/{{ cookiecutter.__package_name }}/{% if cookiecutter.gui -%} gui {%- endif %}/app.py
@@ -0,0 +1,14 @@
+"""Main window."""
+import tkinter as tk
+from tkinter import ttk
+
+
+class App(tk.Tk):
+
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.title("{{ cookiecutter.project_name }}")
+        self.initialize()
+
+    def initialize(self) -> None:
+        """Initialize the widgets."""
